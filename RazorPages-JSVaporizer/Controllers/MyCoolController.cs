4b99062e20f2be487eb9f0de0a918750556fca48
PardusLaboratories <labs@pardus.dev>
2025-03-25 07:52:18 -0400

    Refactor

---
+++ RazorPages-JSVaporizer/Controllers/MyCoolController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using JSVTransformer;
+
+namespace RazorPagesJSVaporizer
+{
+
+    [ApiController]
+    [Route("MyCoolController")]
+    public class MyCoolController : Controller
+    {
+
+        [HttpPost("MyRequestHandler")]
+        public ActionResult<string> MyRequestHandler([FromForm] string dtoJSON)
+        {
+
+            MyCoolTransformer xformer = new MyCoolTransformer();
+            MyCoolTransformerDto dto = xformer.JsonToDto(dtoJSON);
+
+            bool valid = xformer.ValidateDto(dto, out string errMessage);
+            if (valid)
+            {
+                return "You just booked an apointment.";
+            }
+            else
+            {
+                return "You failed to book and appointment, because: " + errMessage; ;
+            }
+            
+        }
+    }
+}
---
+++ RazorPages-JSVaporizer/wwwroot/js/index.js
@@ -0,0 +1,57 @@
+﻿"use strict";
+
+let jsvExportConfig;
+let jsvExports;
+let jsvRegisterCustomImports;
+let jsvRegisterJSFunction;
+let jsvCallJSVGenericFunction;
+let jsvGetExportedAssembly;
+
+import("../jsvwasm/jsvwasm.js").then((jsvWasm) => {
+    jsvExportConfig = jsvWasm.jsvExportConfig;
+    jsvRegisterCustomImports = jsvWasm.jsvRegisterCustomImports;
+    jsvRegisterJSFunction = jsvWasm.jsvRegisterJSFunction;
+    jsvCallJSVGenericFunction = jsvWasm.callJSVGenericFunction;
+    jsvGetExportedAssembly = jsvWasm.GetExportedAssembly;
+
+    // Launch your front end here
+    doCoolThings();
+});
+
+async function doCoolThings() {
+
+    // Register any (hopefully small!) one-off JS functions you need to.
+    // Maybe you need to do this for a quick fix.
+    // Don't forget: Quick fixes have a strange way of becoming permanent spaghetti code.
+    //
+    // Remember: The goal is to REDUCE the amount of BS JS.
+
+    jsvRegisterJSFunction("AjaxPOST", AjaxPOST);
+
+    let dtoJSON = $("#hfDtoJSON").val();
+
+    jsvExports = await jsvGetExportedAssembly("JSVTransformer");
+
+    let resStr = jsvExports.JSVTransformer.TransformerInvoker.Invoke("MyCoolTransformerV1", dtoJSON);
+
+    alert(resStr);
+}
+
+function AjaxPOST(url, dtoJSON, successFuncKey, errorFuncKey) {
+    var payload = new FormData();
+    payload.append("dtoJSON", dtoJSON);
+
+    $.ajax({
+        type: "POST",
+        url: url,
+        contentType: false,
+        processData: false,
+        data: payload,
+        success: function (result) {
+            jsvCallJSVGenericFunction(successFuncKey, result);
+        },
+        error: function (err) {
+            jsvCallJSVGenericFunction(errorFuncKey, err);
+        }
+    });
+}
